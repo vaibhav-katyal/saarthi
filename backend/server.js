da487ad4f896597633b99d27435f2aa4c3ded258
Vaibhav Katyal <vaibhav0480.becse24@chitkara.edu.in>
2026-03-04 01:39:44 +0530

    Setted up the whole production level backend structure and mongodb integration

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+// Mount routers
+// app.use('/api/v1/users', require('./routes/users'));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Saarthi API' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
